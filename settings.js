01c750a58166468a93c01f531bc19dbf0163ddb4
vasudev-gg <vasudev664u@gmail.com>
2025-04-17 22:53:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'ghost',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "ghost Bot",
+  botOwner: 'vasudev', // Your name
+  ownerNumber: '918301966072', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
